665cbe28133a35eef73d0536d9f171a7bb13395c
Arianemld <at>
2024-04-15 00:20:47 +0200

    modification de la fonction sommet_arc, faire verif_ordo. c'est le bon commit

--- verif_ordo.py
+++ verif_ordo.py
@@ -8,26 +8,6 @@ def verif_ordo(choix_fichier):
     print("Point d'entrée :", point_entree)
     print("Point de sortie :", point_sortie)
 
-    sommets_restants = list(range(point_entree, point_sortie + 1))
-    print("Sommets restants :", ' '.join(map(str, sommets_restants)))
 
-    while len(sommets_restants) > 0:
-        print("\nPoints d'entrée :", ' '.join(map(str, sommets_restants)))
-        print("Suppression des points d'entrée")
-        arcs = trace_arc(sommets, choix_fichier)  # Correction ici
-        voisins = set()
-        for arc in arcs:
-            if arc[0] in sommets_restants:
-                voisins.add(arc[1])
-        sommets_restants = list(voisins)
-        print("Sommets restants :", ' '.join(map(str, sommets_restants)))
-
-    if point_sortie in sommets_restants:
-        print("-> Il n’y a pas de circuit")
-    else:
-        print("-> Il y a un circuit")
-
-    print("Il n’y a pas d’arcs négatifs")
-    print("-> C’est un graphe d’ordonnancement")
 
     return point_entree, point_sortie
